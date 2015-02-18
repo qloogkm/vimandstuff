e1918f9a61abfbbe23a7250ecab27292cf7235df
Kevin McCoy <kevin@qloog.com>
2015-02-18 12:24:29 +0900

    Updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+# First we need to install Pathogen
+git clone https://github.com/tpope/vim-pathogen.git;
+pushd vim-pathogen;
+mkdir -p ~/.vim
+mkdir -p ~/.vim/bundle
+mv autoload ~/.vim/
+popd;
+
+# Next install Indent Guides
+pushd ~/.vim/bundle
+git clone https://github.com/nathanaelkane/vim-indent-guides.git
+popd
+
+# Finally copy .vimrc
 cp ./.vimrc ~/
 echo "Installation complete!"
